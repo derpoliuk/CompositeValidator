5910c37b6a57d306e1be68fa063a4b0ffc8c4080
derpoliuk <derpoliuk@gmail.com>
2016-11-29 10:13:39 +0200

    Add functional validators

--- CompositeValidator.playground/Contents.swift
+++ CompositeValidator.playground/Contents.swift
@@ -8,13 +8,25 @@ import UIKit
  We'll use Functional Swift book to build functional version of composite validator
  */
 
-enum ValidatorResult {
+// MARK: - Constants for testing
+
+let emptyEmail = ""
+let invalidEmail = "invalidEmail@"
+let validEmail = "validEmail@validDomain.com"
+let emptyPassword = ""
+let tooShortPassword = "pa$$"
+let passwordWithNoUppercase = "passw0rd"
+let validPassword = "paSSw0rd"
+
+// MAKR: - Original validators
+
+enum ValidatorResult1 {
     case valid
     case invalid(error: Error)
 }
 
-protocol Validator {
-    func validate(string: String) -> ValidatorResult
+protocol Validator1 {
+    func validate(string: String) -> ValidatorResult1
 }
 
 enum EmailValidatorError: Error {
@@ -30,7 +42,7 @@ enum PasswordValidatorError: Error {
     case noNumber
 }
 
-struct EmptyStringValidator: Validator {
+struct EmptyStringValidator: Validator1 {
 
     // This error is passed via the initializer to allow this validator to be reused
     private let invalidError: Error
@@ -39,15 +51,15 @@ struct EmptyStringValidator: Validator {
         self.invalidError = invalidError
     }
 
-    func validate(string: String) -> ValidatorResult {
+    func validate(string: String) -> ValidatorResult1 {
         return string.isEmpty ? .invalid(error: invalidError) : .valid
     }
 
 }
 
-struct EmailFormatValidator: Validator {
+struct EmailFormatValidator: Validator1 {
 
-    func validate(string: String) -> ValidatorResult {
+    func validate(string: String) -> ValidatorResult1 {
         let magicEmailRegexStolenFromTheInternet = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
         let emailTest = NSPredicate(format: "SELF MATCHES %@", magicEmailRegexStolenFromTheInternet)
         return emailTest.evaluate(with: string) ? .valid : .invalid(error: EmailValidatorError.invalidFormat)
@@ -55,17 +67,17 @@ struct EmailFormatValidator: Validator {
 
 }
 
-struct PasswordLengthValidator: Validator {
+struct PasswordLengthValidator: Validator1 {
 
-    func validate(string: String) -> ValidatorResult {
+    func validate(string: String) -> ValidatorResult1 {
         return string.characters.count >= 8 ? .valid : .invalid(error: PasswordValidatorError.tooShort)
     }
 
 }
 
-struct UppercaseLetterValidator: Validator {
+struct UppercaseLetterValidator: Validator1 {
 
-    func validate(string: String) -> ValidatorResult {
+    func validate(string: String) -> ValidatorResult1 {
         let uppercaseLetterRegex = ".*[A-Z]+.*"
         let uppercaseLetterTest = NSPredicate(format: "SELF MATCHES %@", uppercaseLetterRegex)
         return uppercaseLetterTest.evaluate(with: string) ? .valid : .invalid(error: PasswordValidatorError.noUppercaseLetter)
@@ -73,9 +85,9 @@ struct UppercaseLetterValidator: Validator {
     
 }
 
-struct LowercaseLetterValidator: Validator {
+struct LowercaseLetterValidator: Validator1 {
 
-    func validate(string: String) -> ValidatorResult {
+    func validate(string: String) -> ValidatorResult1 {
         let lowercaseLetterRegex = ".*[a-z]+.*"
         let lowercaseLetterTest = NSPredicate(format: "SELF MATCHES %@", lowercaseLetterRegex)
         return lowercaseLetterTest.evaluate(with: string) ? .valid : .invalid(error: PasswordValidatorError.noLowercaseLetter)
@@ -83,9 +95,9 @@ struct LowercaseLetterValidator: Validator {
     
 }
 
-struct ContainsNumberValidator: Validator {
+struct ContainsNumberValidator: Validator1 {
 
-    func validate(string: String) -> ValidatorResult {
+    func validate(string: String) -> ValidatorResult1 {
         let containsNumberRegex = ".*[0-9]+.*"
         let containsNumberTest = NSPredicate(format: "SELF MATCHES %@", containsNumberRegex)
         return containsNumberTest.evaluate(with: string) ? .valid : .invalid(error: PasswordValidatorError.noNumber)
@@ -93,15 +105,15 @@ struct ContainsNumberValidator: Validator {
 
 }
 
-struct CompositeValidator: Validator {
+struct CompositeValidator: Validator1 {
 
-    private let validators: [Validator]
+    private let validators: [Validator1]
 
-    init(validators: [Validator]) {
+    init(validators: [Validator1]) {
         self.validators = validators
     }
 
-    func validate(string: String) -> ValidatorResult {
+    func validate(string: String) -> ValidatorResult1 {
         for validator in validators {
             switch validator.validate(string: string) {
             case .invalid(let error):
@@ -115,30 +127,30 @@ struct CompositeValidator: Validator {
 
 }
 
-struct ValidatorConfigurator {
+struct ValidatorConfigurator1 {
 
-    static func emailValidator() -> Validator {
-        let validators: [Validator] = [emptyEmailStringValidator(), EmailFormatValidator()]
+    static func emailValidator() -> Validator1 {
+        let validators: [Validator1] = [emptyEmailStringValidator(), EmailFormatValidator()]
         return CompositeValidator(validators: validators)
     }
 
-    static func passwordValidator() -> Validator {
-        let validators: [Validator] = [emptyPasswordStringValidator(), passwordStrengthValidator()]
+    static func passwordValidator() -> Validator1 {
+        let validators: [Validator1] = [emptyPasswordStringValidator(), passwordStrengthValidator()]
         return CompositeValidator(validators: validators)
     }
 
     // MAKR: Private
 
-    private static func emptyEmailStringValidator() -> Validator {
+    private static func emptyEmailStringValidator() -> Validator1 {
         return EmptyStringValidator(invalidError: EmailValidatorError.empty)
     }
 
-    private static func emptyPasswordStringValidator() -> Validator {
+    private static func emptyPasswordStringValidator() -> Validator1 {
         return EmptyStringValidator(invalidError: PasswordValidatorError.empty)
     }
 
-    private static func passwordStrengthValidator() -> Validator {
-        let validators: [Validator] = [PasswordLengthValidator(),
+    private static func passwordStrengthValidator() -> Validator1 {
+        let validators: [Validator1] = [PasswordLengthValidator(),
                                        UppercaseLetterValidator(),
                                        LowercaseLetterValidator(),
                                        PasswordLengthValidator()]
@@ -147,16 +159,121 @@ struct ValidatorConfigurator {
 
 }
 
-// MARK: Testing
+// MARK: - Testing
 
-let emailValidator = ValidatorConfigurator.emailValidator()
-let passwordValidator = ValidatorConfigurator.passwordValidator()
+let emailValidator1 = ValidatorConfigurator1.emailValidator()
+let passwordValidator1 = ValidatorConfigurator1.passwordValidator()
 
-print(emailValidator.validate(string: ""))
-print(emailValidator.validate(string: "invalidEmail@"))
-print(emailValidator.validate(string: "validEmail@validDomain.com"))
+print(emailValidator1.validate(string: emptyEmail))
+print(emailValidator1.validate(string: invalidEmail))
+print(emailValidator1.validate(string: validEmail))
 
-print(passwordValidator.validate(string: ""))
-print(passwordValidator.validate(string: "psS$"))
-print(passwordValidator.validate(string: "passw0rd"))
-print(passwordValidator.validate(string: "paSSw0rd"))
+print(passwordValidator1.validate(string: emptyPassword))
+print(passwordValidator1.validate(string: tooShortPassword))
+print(passwordValidator1.validate(string: passwordWithNoUppercase))
+print(passwordValidator1.validate(string: validPassword))
+
+// MARK: - Functional validators
+
+enum ValidatorResult2 {
+    case valid
+    case invalid(error: Error)
+
+    // TODO: rename
+    func compose(_ validatorResult: ValidatorResult2) -> ValidatorResult2 {
+        switch (self, validatorResult) {
+        case (.valid, .valid):
+            return .valid
+        case (.invalid(let error), _):
+            return .invalid(error: error)
+        case (_, .invalid(let error)):
+            return .invalid(error: error)
+        default:
+            return .valid // just for testing
+        }
+    }
+}
+
+typealias Validator2 = (String) -> ValidatorResult2
+
+func compose(val1: @escaping Validator2, val2: @escaping Validator2) -> Validator2 {
+    return { string in val1(string).compose(val2(string)) }
+}
+
+func emptyStringValidator(error: Error) -> Validator2 {
+    return { string in string.isEmpty ? .invalid(error: error) : .valid }
+}
+
+func emptyEmailStringValidator() -> Validator2 {
+    return emptyStringValidator(error: EmailValidatorError.empty)
+}
+
+func emptyPasswordValidator() -> Validator2 {
+    return emptyStringValidator(error: PasswordValidatorError.empty)
+}
+
+func emailFormatValidator() -> Validator2 {
+    return { string in
+        let magicEmailRegexStolenFromTheInternet = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+        let emailTest = NSPredicate(format: "SELF MATCHES %@", magicEmailRegexStolenFromTheInternet)
+        return emailTest.evaluate(with: string) ? .valid : .invalid(error: EmailValidatorError.invalidFormat)
+    }
+}
+
+func passwordLengthValidator() -> Validator2 {
+    return { string in string.characters.count >= 8 ? .valid : .invalid(error: PasswordValidatorError.tooShort) }
+}
+
+func passwordUppercaseValidator() -> Validator2 {
+    return { string in
+        let uppercaseLetterRegex = ".*[A-Z]+.*"
+        let uppercaseLetterTest = NSPredicate(format: "SELF MATCHES %@", uppercaseLetterRegex)
+        return uppercaseLetterTest.evaluate(with: string) ? .valid : .invalid(error: PasswordValidatorError.noUppercaseLetter)
+    }
+}
+
+func passwordLowercaseValidator() -> Validator2 {
+    return { string in
+        let lowercaseLetterRegex = ".*[a-z]+.*"
+        let lowercaseLetterTest = NSPredicate(format: "SELF MATCHES %@", lowercaseLetterRegex)
+        return lowercaseLetterTest.evaluate(with: string) ? .valid : .invalid(error: PasswordValidatorError.noLowercaseLetter)
+    }
+}
+
+func containsNumberValidator() -> Validator2 {
+    return { string in
+        let containsNumberRegex = ".*[0-9]+.*"
+        let containsNumberTest = NSPredicate(format: "SELF MATCHES %@", containsNumberRegex)
+        return containsNumberTest.evaluate(with: string) ? .valid : .invalid(error: PasswordValidatorError.noNumber)
+    }
+}
+
+func &(_ validator1: @escaping Validator2, _ validator2: @escaping Validator2) -> Validator2 {
+    return { string in validator1(string).compose(validator2(string)) }
+}
+
+struct ValidatorConfigurator2 {
+
+    static func emailValidator() -> Validator2 {
+        return emptyEmailStringValidator() & emailFormatValidator()
+    }
+
+    static func passwordValidator() -> Validator2 {
+        return emptyPasswordValidator() & passwordLengthValidator() & passwordUppercaseValidator() & passwordLowercaseValidator() & containsNumberValidator()
+    }
+
+}
+
+// MARK: - Testing
+
+let emailValidator2 = ValidatorConfigurator2.emailValidator()
+print(emailValidator2(emptyEmail))
+print(emailValidator2(invalidEmail))
+print(emailValidator2(validEmail))
+
+let passwordValidator2 = ValidatorConfigurator2.passwordValidator()
+print(passwordValidator2(emptyPassword))
+print(passwordValidator2(tooShortPassword))
+print(passwordValidator2(passwordWithNoUppercase))
+print(passwordValidator2(validPassword))
+
